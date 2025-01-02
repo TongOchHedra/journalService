a1e1cba6f12449ef7b20206c0dd0d405e5a8e2a8
Tongthebread <tongschannel@gmail.com>
2025-01-02 13:16:09 +0100

    cors conf and dockerfile

--- JournalService/src/main/java/com/example/journalservice/Security/CorsConfig.java
+++ JournalService/src/main/java/com/example/journalservice/Security/CorsConfig.java
@@ -8,10 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
